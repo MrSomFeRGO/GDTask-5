c1cb2f777a9e48601a2d79f8956e56dcba21f982
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 17:46:26 +0300

    fix git

--- GDTask-5/Program.cs
+++ GDTask-5/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿List<int> input = new List<int>()
+    { 4, 3, 3, 4, 5, 6, 1, 4 };
+foreach (var el in remove_buble(input))
+{
+    Console.Write($"{el} ");
+}
 
-Console.WriteLine("Hello, World!");
+static List<int> remove_buble(List<int> input)
+{
+    List<int> output = new List<int>();
+    foreach (var element in input)
+    {
+        if (!output.Contains(element))
+        {
+            output.Add(element);
+        }
+    }
+
+    return output;
+}
